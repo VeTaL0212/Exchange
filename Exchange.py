f15acff331b7d81e77d9a8998e92dae34a3d5765
Vitali <vitaly.u@mail.ru>
2024-11-22 20:50:20 +0300

    Создана функция exchange для получения из интернета курсов валют

--- Exchange.py
+++ Exchange.py
@@ -4,7 +4,23 @@ from tkinter import *
 from tkinter import messagebox as mb
 
 
+def exchange():
+    code = entry.get()
 
+    if code:
+        try:
+            response = requests.get("https://open.er-api.com/v6/latest/USD") # запрос к сайту
+            response.raise_for_status() # проверяем на ошибки
+            data = response.json() # раскладываем в виде обычного python словаря
+            if code in data["rates"]:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за один доллар")
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание", "Введите код калюты")
 
 
 window = Tk()
