00ee253be57f0012e2b57bb6f8e41a76a2148337
Vitali <vitaly.u@mail.ru>
2024-11-22 23:05:11 +0300

    добавлена метка с названием базовой валюты

--- Exchange.py
+++ Exchange.py
@@ -78,6 +78,7 @@ Button(text="Получить курс обмена", command=exchange).pack(pad
 window.mainloop()
 
 
+
 # result = requests.get('https://open.er-api.com/v6/latest/USD')
 # data = json.loads(result.text)
 # p = pprint.PrettyPrinter(indent=4)
